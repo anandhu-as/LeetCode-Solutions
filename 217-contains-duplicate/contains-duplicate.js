a4744c1154d18dda34ac34e7dd0deba471e97dd1
Anandhu as <109361830+anandhu-as@users.noreply.github.com>
2026-07-15 09:14:11 +0530

    Time: 27 ms (31.85%) | Memory: 78.8 MB (31.43%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -2,6 +2,10 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+ //Set stores array without duplicate elements
+ // so length of array will be  3
+ //length of orginal array 4
+ //3!=4 returns true
  // 1 2 3 5 1
 const containsDuplicate = function(nums) {
     return new Set(nums).size !== nums.length;
